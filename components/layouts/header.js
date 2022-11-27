794f06900289788604087d40be36f0ba0e33394b
sthakur3 <lszu9veBTn1o1>
2022-11-28 01:31:04 +0530

    changes

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -17,7 +17,6 @@ const Header = () => {
         {label: "About us", link: "/about-us"},
         {label: "Contact us", link: "/contact-us"},
         {label: "Careers", link: "/careers"},
-        {label: "Company Details", link: "company-details"},
         {label: "User", link: "", children: [
           {label: "Sign up", link: "/account"},
           {label: "Login", link: "/logout"},
@@ -25,6 +24,11 @@ const Header = () => {
           {label: "Help", link: "/logout"},
           {label: "Term and Conditions", link: "/logout"},
         ]},
+    ];
+
+    let secondary_nav_links = [
+        {label: "Sign In", link: ""},
+        {label: "Join Us", link: "/about-us"}
     ];
 
     const [navScrolled, setNavScrolled] = useState(false);
@@ -48,59 +52,19 @@ const Header = () => {
     }, []);
 
     return (
-        <div className={"container m-auto px-6 sticky top-0 border-b-[1px] border-gray-600 " + ((navScrolled) ? "bg-slate-700" : "")}>
+        <div className={"container m-auto px-6 sticky top-0 border-b-[1px] border-gray-600 " + ((navScrolled) ? "bg-gray-700" : "")}>
             <div className={navScrolled ? "py-4 transition-all duration-300" : "py-6 transition-all duration-300"}>
                 <header className="flex items-center justify-between">
                     <div id="logo"><Image src="/logo.svg" alt="Site Logo" width={40} height={40} /></div>
 
                     <nav>
+
                         <div className={"hidden md:flex justify-between space-x-6 font-semibold relative text-left "  + ((navScrolled) ? "text-white" : "")}>
-                            {nav_links.map((link, i) => {
-                                if("children" in link) {
-                                  var menu = (
-                                    <DropdownMenu.Root key={i}>
-                                        <DropdownMenu.Trigger asChild>
-                                            <button className="IconButton" aria-label="Customise options">
-                                            {link.label}
-                                            </button>
-                                        </DropdownMenu.Trigger>
-
-                                        <DropdownMenu.Portal>
-                                            <DropdownMenu.Content className="min-w-[10em] md:min-w-[12em] rounded-lg py-2 shadow-2xl bg-white" sideOffset={5}>
-                                            {link.children.map((child_link, i) => {
-                                                var sub_menu;
-                                                if(child_link.type && child_link.type == "seperator") {
-                                                    sub_menu = (
-                                                        <DropdownMenu.Separator className="my-1 h-px bg-gray-200" key={i}/>
-                                                    );
-                                                } else {
-                                                    sub_menu = (
-                                                        <DropdownMenu.Item className="flex cursor-default select-none items-center py-2 outline-none text-sm focus:bg-gray-200 " key={i}>
-                                                            <span className="flex-grow px-4">
-                                                                {child_link.label}
-                                                            </span>
-                                                        </DropdownMenu.Item>
-                                                    );
-                                                }
-
-                                                return sub_menu;
-                                            })}
-                                            </DropdownMenu.Content>
-                                        </DropdownMenu.Portal>
-                                    </DropdownMenu.Root>
-                                  )
-                                } else {
-                                    var menu = (
-                                        <span className={" " + ((link.class) ? link.class: "")} key={i}>
-                                            {(link.link) ? (<Link href={link.link} className={"text-lg " + ((navScrolled) ? "link_inverted" : "link")}>{link.label}</Link>) : link.label}
-                                        </span>
-                                    )
-                                }
-                                return (
-                                    menu
-                                );
-                            })}
+                            <MenuNav links={nav_links} scrolled={navScrolled}/>
                         </div>
+                        
+
+
                         <div className="md:hidden">
                             <HamburgerMenuIcon onClick={() => setMobileNavOpen(!mobileNavOpen)}/>
                             <div className={"fixed w-full left-0 right-0 min-h-screen bg-gray-400 transition-all duration-300 transform " + ((mobileNavOpen) ? "translate-x-0" : "translate-x-full")}>
@@ -119,6 +83,61 @@ const Header = () => {
             </div>
         </div>
     );
+}
+
+const MenuNav = (props) => {
+
+    const {links, scrolled} = props;
+
+    return (
+        <>
+        {links.map((link, i) => {
+            if("children" in link) {
+                var menu = (
+                <DropdownMenu.Root key={i}>
+                    <DropdownMenu.Trigger asChild>
+                        <button className="IconButton" aria-label="Customise options">
+                        {link.label}
+                        </button>
+                    </DropdownMenu.Trigger>
+
+                    <DropdownMenu.Portal>
+                        <DropdownMenu.Content className="min-w-[10em] md:min-w-[12em] rounded-lg py-2 shadow-2xl bg-white" sideOffset={5}>
+                        {link.children.map((child_link, i) => {
+                            var sub_menu;
+                            if(child_link.type && child_link.type == "seperator") {
+                                sub_menu = (
+                                    <DropdownMenu.Separator className="my-1 h-px bg-gray-200" key={i}/>
+                                );
+                            } else {
+                                sub_menu = (
+                                    <DropdownMenu.Item className="flex cursor-default select-none items-center py-2 outline-none text-sm focus:bg-gray-200 " key={i}>
+                                        <span className="flex-grow px-4">
+                                            {child_link.label}
+                                        </span>
+                                    </DropdownMenu.Item>
+                                );
+                            }
+
+                            return sub_menu;
+                        })}
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Portal>
+                </DropdownMenu.Root>
+                )
+            } else {
+                var menu = (
+                    <span className={((link.class) ? link.class: "") + "text-lg " + ((scrolled) ? "link_inverted" : "link")} key={i}>
+                        {(link.link) ? (<Link href={link.link}>{link.label}</Link>) : link.label}
+                    </span>
+                )
+            }
+            return (
+                menu
+            );
+        })}
+        </>
+    )
 }
 
 export default Header;
